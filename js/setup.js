f78a3ded2e67d2c93003c1bac6685232b780d7e4
Viktor_Black <relax-elite@mail.ru>
2020-10-08 20:36:55 +0300

    for the Horde-2

--- js/setup.js
+++ js/setup.js
@@ -35,32 +35,14 @@ const WIZARD_EYES_COLOR = [
   `yellow`,
   `green`
 ];
-const wizards = [
-  {
-    name: WIZARD_NAMES[0],
-    lastname: WIZARD_LASTNAMES[0],
-    coatColor: WIZARD_COAT_COLOR[0],
-    eyesColor: WIZARD_EYES_COLOR[0]
-  },
-  {
-    name: WIZARD_NAMES[1],
-    lastname: WIZARD_LASTNAMES[1],
-    coatColor: WIZARD_COAT_COLOR[1],
-    eyesColor: WIZARD_EYES_COLOR[1]
-  },
-  {
-    name: WIZARD_NAMES[2],
-    lastname: WIZARD_LASTNAMES[2],
-    coatColor: WIZARD_COAT_COLOR[2],
-    eyesColor: WIZARD_EYES_COLOR[2]
-  },
-  {
-    name: WIZARD_NAMES[3],
-    lastname: WIZARD_LASTNAMES[3],
-    coatColor: WIZARD_COAT_COLOR[3],
-    eyesColor: WIZARD_EYES_COLOR[3]
-  }
-];
+const wizards = {
+  name: WIZARD_NAMES,
+  lastname: WIZARD_LASTNAMES,
+  coatColor: WIZARD_COAT_COLOR,
+  eyesColor: WIZARD_EYES_COLOR
+};
+const amountWizards = 4;
+
 const userDialog = document.querySelector(`.setup`);
 const similarСharacters = document.querySelector(`.setup-similar`);
 const similarListElement = document.querySelector('.setup-similar-list');
@@ -77,7 +59,7 @@ const activeMode = (element) => {
 activeMode(userDialog);
 activeMode(similarСharacters);
 
-var renderWizard = function () {
+const renderWizard = function () {
   let wizardElement = similarWizardTemplate.cloneNode(true);
 
   wizardElement.querySelector('.setup-similar-label').textContent = `${getRandomData(WIZARD_NAMES)} ${getRandomData(WIZARD_LASTNAMES)}`;
@@ -89,10 +71,16 @@ var renderWizard = function () {
   return wizardElement;
 };
 
-let fragment = document.createDocumentFragment();
+const createNodeFragment = () => {
+  let fragment = document.createDocumentFragment();
 
-for (var i = 0; i < wizards.length; i++) {
-  fragment.appendChild(renderWizard(wizards[i]));
-}
+  for (var i = 0; i < amountWizards; i++) {
+    fragment.appendChild(renderWizard(wizards[i]));
+  }
 
-similarListElement.appendChild(fragment);
+  similarListElement.appendChild(fragment);
+
+  return createNodeFragment;
+};
+
+createNodeFragment();
